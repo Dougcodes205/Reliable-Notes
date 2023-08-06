16e5ab294c87c0bc912adc75c601929873c07556
dougcodes <tannercollege6@gmail.com>
2023-08-06 19:19:28 -0400

    finito

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,64 +3,62 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js',
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './src/index.html',
-        filename: 'index.html',
-        chunks: ['main'],
-      }),
-      new HtmlWebpackPlugin({
-        template: './src/install.html',
-        filename: 'install.html',
-        chunks: ['install'],
-      }),
-      new WebpackPwaManifest({
-        name: 'J.A.T.E',
-        short_name: 'J.A.T.E',
-        description: 'Just Another Text Editor',
-        background_color: '#272822',
-        theme_color: '#31a9e1',
-        start_url: '/',
-        icons: [
-          {
-            src: path.resolve('src/assets/icons/icon_96x96.png'),
-            sizes: [96, 128, 192, 256, 384, 512],
-            purpose: 'any maskable',
-          },
-        ],
-      }),
-      new InjectManifest({
-        swSrc: './src-sw.js',
-      }),
-    ],
-    module: {
-      rules: [
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './client/src/js/index.js',
+    install: './client/src/js/install.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'client/dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './client/index.html',
+      filename: 'index.html',
+      chunks: ['main'],
+    }),
+    new HtmlWebpackPlugin({
+      template: './client/install.html',
+      filename: 'install.html',
+      chunks: ['install'],
+    }),
+    new WebpackPwaManifest({
+      name: 'J.A.T.E',
+      short_name: 'J.A.T.E',
+      description: 'Just Another Text Editor',
+      background_color: '#272822',
+      theme_color: '#31a9e1',
+      start_url: '/',
+      icons: [
         {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
-        },
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env'],
-            },
-          },
+          src: path.resolve(__dirname, 'client/src/assets/icons/icon_96x96.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          purpose: 'any maskable',
         },
       ],
-    },
-  };
+    }),
+    new InjectManifest({
+      swSrc: './client/src-sw.js',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
 };
