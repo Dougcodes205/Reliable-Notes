bb0213c8ddda2bc51b2c14d7032e0f7e169c3b8f
dougcodes <tannercollege6@gmail.com>
2023-08-06 18:30:21 -0400

    db,inst,src,wbpk

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,14 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const db = await initdb();
+  return db.put('jate', { content });
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const db = await initdb();
+  return db.getAll('jate');
+};
 
 initdb();
